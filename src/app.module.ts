07c2a246df3ac2ca562ceebefbe03195cfc0bed3
maaaaaaaaad <integral5137@gmail.com>
2022-04-04 10:44:18 +0900

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import * as Joi from 'joi'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'development'
+          ? '.development.env'
+          : '.production.env',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
