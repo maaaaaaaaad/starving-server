1940a077058891d18432a44814c9fa73e1f0e2a2
maaaaaaaaad <integral5137@gmail.com>
2022-05-11 00:47:37 +0900

    test code - should fail if email already exists

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -5,9 +5,13 @@ import { getRepositoryToken } from '@nestjs/typeorm'
 import { PassportModule } from '@nestjs/passport'
 import { JwtModule, JwtService } from '@nestjs/jwt'
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { Repository } from 'typeorm'
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>
 
 describe('AuthService', () => {
   let service: AuthService
+  let userRepository: MockRepository<UserEntity>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -46,9 +50,27 @@ describe('AuthService', () => {
     }).compile()
 
     service = module.get<AuthService>(AuthService)
+    userRepository = module.get(getRepositoryToken(UserEntity))
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('register user account', () => {
+    it('should fail if email already exists', async () => {
+      userRepository.findOne.mockResolvedValue({
+        email: 'mad@gmail.com',
+      })
+      const result = await service.register({
+        email: 'mad@gmail.com',
+        password: '',
+        nickname: '',
+      })
+      expect(result).toMatchObject({
+        access: false,
+        message: 'Already to this user email',
+      })
+    })
   })
 })
