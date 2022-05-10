0f44e33e087d90614d97cededdf2d617b5f97c11
maaaaaaaaad <integral5137@gmail.com>
2022-05-11 00:08:27 +0900

    test code config that auth service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,54 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { AuthService } from './auth.service'
+import { UserEntity } from './entities/user.entity'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule, JwtService } from '@nestjs/jwt'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let service: AuthService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+      imports: [
+        PassportModule.register({
+          session: false,
+          defaultStrategy: 'jwt',
+        }),
+        JwtModule.registerAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: async (configService: ConfigService) => ({
+            secret: configService.get<string>('JWT_SECRET_KEY'),
+            signOptions: { expiresIn: '1y' },
+          }),
+        }),
+      ],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            findOne: jest.fn(),
+            save: jest.fn(),
+            create: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(JwtService),
+          useValue: {
+            sign: jest.fn(),
+          },
+        },
+      ],
+    }).compile()
 
-    service = module.get<AuthService>(AuthService);
-  });
+    service = module.get<AuthService>(AuthService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
