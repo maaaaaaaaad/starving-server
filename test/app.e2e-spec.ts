19e520a2b1bc46f6b941e183d10a3cad7841b511
maaaaaaaaad <integral5137@gmail.com>
2022-05-16 16:23:30 +0900

    test code - e2e auth register, login

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,69 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication, ValidationPipe } from '@nestjs/common'
+import * as request from 'supertest'
+import { AppModule } from '../src/app.module'
+import { UserRegisterInputDto } from '../src/auth/dtos/user.register.dto'
+import { UserLoginInputDto } from '../src/auth/dtos/user.login.dto'
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: INestApplication
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    }).compile()
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+    app = moduleFixture.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe())
+    await app.init()
+  })
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
+  describe('auth', () => {
+    describe('register', () => {
+      const dto: UserRegisterInputDto = {
+        email: 'mad@gmail.com',
+        password: 'abcabc123123',
+        nickname: 'madmad',
+        social: null,
+        avatarImage: null,
+      }
+      const url = '/auth/register'
+      it('should success', async () => {
+        return request(app.getHttpServer()).post(url).send(dto).expect(201)
+      })
+
+      it('should fail if the email exists', async () => {
+        const res = await request(app.getHttpServer())
+          .post(url)
+          .send(dto)
+          .expect(201)
+        expect(res.body).toMatchObject({
+          access: false,
+          message: 'This email already to exists',
+        })
+      })
+    })
+
+    describe('login', () => {
+      const dto: UserLoginInputDto = {
+        email: 'mad@gmail.com',
+        password: 'abcabc123123',
+      }
+      const url = '/auth/login'
+      it('should success', async () => {
+        return request(app.getHttpServer()).post(url).send(dto).expect(201)
+      })
+
+      it('should fail if the not found user', async () => {
+        const res = await request(app.getHttpServer())
+          .post(url)
+          .send({ email: 'sdofijesf@gmail.com', password: 'weofij123123' })
+          .expect(201)
+        expect(res.body).toMatchObject({
+          access: false,
+          error: 'Not found this user',
+        })
+      })
+    })
+  })
+})
