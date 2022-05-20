26607e1bb13de7cb4c18a0048b43c82317326b95
maaaaaaaaad <integral5137@gmail.com>
2022-05-20 13:34:06 +0900

    test code - re config recipe service spec

--- src/recipe/recipe.service.spec.ts
+++ src/recipe/recipe.service.spec.ts
@@ -3,14 +3,10 @@ import { RecipeService } from './recipe.service'
 import { getRepositoryToken } from '@nestjs/typeorm'
 import { RecipeEntity } from './entities/recipe.entity'
 import { CategoryEntity } from './entities/category.entity'
-import { Connection, Repository } from 'typeorm'
-
-type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>
+import { Connection } from 'typeorm'
 
 describe('RecipeService', () => {
   let service: RecipeService
-  let recipeRepository: MockRepository<RecipeEntity>
-  let categoryRepository: MockRepository<CategoryEntity>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,37 +15,36 @@ describe('RecipeService', () => {
         {
           provide: getRepositoryToken(RecipeEntity),
           useValue: {
-            findAndCount: jest.fn(),
             findOne: jest.fn(),
             save: jest.fn(),
+            create: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(CategoryEntity),
+          useValue: {
+            findOne: jest.fn(),
+            save: jest.fn(),
+            create: jest.fn(),
             delete: jest.fn(),
           },
         },
         {
           provide: Connection,
           useValue: {
-            createQueryRunner: jest.fn(),
-            connect: jest.fn(),
-            startTransaction: jest.fn(),
-            getRepository: jest.fn(),
-            create: jest.fn(),
             findOne: jest.fn(),
             save: jest.fn(),
-            commitTransaction: jest.fn(),
-            rollbackTransaction: jest.fn(),
-            release: jest.fn(),
+            create: jest.fn(),
+            delete: jest.fn(),
           },
         },
       ],
     }).compile()
     service = module.get<RecipeService>(RecipeService)
-    recipeRepository = module.get(getRepositoryToken(RecipeEntity))
-    categoryRepository = module.get(getRepositoryToken(CategoryEntity))
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
-    expect(recipeRepository).toBeDefined()
-    expect(categoryRepository).toBeDefined()
   })
 })
