8a32caaa7772f4a32d4103b9f2ed96e25ef1016a
maaaaaaaaad <integral5137@gmail.com>
2022-05-24 23:20:39 +0900

    config typeorm & joi required

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -2,23 +2,29 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 
 const ormConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: +process.env.POSTGRES_PORT,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  ...(process.env.DATABASE_URL
+    ? { url: process.env.DATABASE_URL }
+    : {
+        host: process.env.POSTGRES_HOST,
+        port: +process.env.POSTGRES_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+      }),
   entities: [__dirname + '/*/entities/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: process.env.NODE_ENV !== 'production',
   logging: true,
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   cli: {
     migrationsDir: 'src/migrations',
   },
   migrationsRun: false,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
+  ...(process.env.NODE_ENV === 'production' && {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  }),
 }
 
 export = ormConfig
