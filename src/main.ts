8f838059706e62f96eccbdb8e9cc7f0b6d5c8e02
maaaaaaaaad <integral5137@gmail.com>
2022-07-12 21:20:14 +0900

    set up winston

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,36 @@ import { UndefinedInterceptor } from './common/interceptors/undefined.intercepto
 import * as path from 'path'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import helmet from 'helmet'
+import * as winston from 'winston'
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike('MyApp', {
+              prettyPrint: true,
+            }),
+          ),
+        }),
+        new winston.transports.File({
+          filename: 'combined.log',
+          level: 'info',
+        }),
+        new winston.transports.File({
+          filename: 'errors.log',
+          level: 'error',
+        }),
+      ],
+    }),
+  })
   app.enableCors({
     origin: process.env.ORIGIN_URL,
     credentials: true,
